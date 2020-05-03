b58e66ead0ce6e76cf2c7fb4272bbb16525e996a
karenbennis <bennis.karen@gmail.com>
2020-05-02 20:22:05 -0400

    Updated working files

--- apps/scraping.py
+++ apps/scraping.py
@@ -52,12 +52,12 @@ def mars_news(browser):
         news_title
 
         # Use the parent element to find the paragraph text
-        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+        news_paragraph = slide_elem.find('div', class_="article_teaser_body").get_text()
 
     except AttributeError:
         return None, None
 
-    return news_title, news_p
+    return news_title, news_paragraph
 
 
 
@@ -93,6 +93,37 @@ def featured_image(browser):
     img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
     return img_url
 
+def featured_image(browser):
+
+    # Visit URL
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
+
+    # Find the more info button and click that
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.find_link_by_partial_text('more info')
+    more_info_elem.click()
+
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = BeautifulSoup(html, 'html.parser')
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+    
+    except AttributeError:
+        return None
+
+    # Use the base URL to create an absolute URL
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+    return img_url
+
+
 def mars_facts():
 
     try:
@@ -107,11 +138,16 @@ def mars_facts():
     df.set_index('Description', inplace=True)
     
     return df.to_html()
-    
+
+ #### CHALLENGE#############   
+
+
+
+
+
+
     if __name__ == "__main__":
         # If running as script, print scraped data
         print(scrape_all())
 
-
-
-#browser.quit()
+browser.quit()
